01572c1df124237e6659f4cd2814303339041326
Adam Minchella <adamminchella@msn.com>
2022-04-20 14:47:33 +0100

    Create opearate function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+function operate(operator, x, y) {
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+    default:
+      "fuk u";
+  }
+}
+
+console.log(operate("+", 4, 2));
